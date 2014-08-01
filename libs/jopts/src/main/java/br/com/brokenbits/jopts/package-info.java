9265937f98502d1c406bb867da930c7fb341806f
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2014-08-01 07:23:05 -0300

    Initial version  of the package-info.

---
+++ libs/jopts/src/main/java/br/com/brokenbits/jopts/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all classes of JOpts library. 
+ * 
+ * <p>JOpts is a command line parser that uses annotations to define the
+ * command line arguments.
+ */
+package br.com.brokenbits.jopts;
